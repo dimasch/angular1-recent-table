c9e16b085bb3f6160262d61f2b7a6f6eb79822df
schegolihin_da <d.shegolihin@gmail.com>
2013-07-17 14:55:50 +0400

    Add Ru locale. Test date sorting.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,19 +3,21 @@
 angular.module('angularTableApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      {id: 0, name: 'HTML5 Boilerplate'},
-      {id: 1, name: 'AngularJS'},
-      {id: 2, name: 'Karma'},            
+      {id: 0, name: 'HTML5 Boilerplate', start: '1288323623006'},
+      {id: 1, name: 'AngularJS', start: new Date() },
+      {id: 2, name: 'Karma', start: '1288323623006'},            
       {id: 3, name: 'HTML5 Boilerplate'},
-      {id: 4, name: 'AngularJS'},
+      {id: 4, name: 'AngularJS', start: '1288323623006'},
       {id: 5, name: 'Karma'},
       {id: 6, name: 'HTML5 Boilerplate'},
-      {id: 7, name: 'AngularJS'},
+      {id: 7, name: 'AngularJS', start: '1288323623006'},
       {id: 8, name: 'Karma'}, 
       {id: 9, name: 'HTML5 Boilerplate'},
-      {id: 10, name: 'AngularJS'},
+      {id: 10, name: 'AngularJS', start: '1288323623006'},
       {id: 11, name: 'Karma'} 
     ];
+
+    //debugger;
 
     var vowels = ["a", "e", "i", "o", "u"];
     $scope.vowelsList = function(word) {
