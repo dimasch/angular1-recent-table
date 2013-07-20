c78dd9bb38b076561c04dc4fc5127d6c079987b1
schegolihin_da <d.shegolihin@gmail.com>
2013-07-20 12:50:41 +0400

    Add controller to tickets directive.

--- app/scripts/directives/tickets.js
+++ app/scripts/directives/tickets.js
@@ -4,7 +4,21 @@ angular.module('angularTableApp')
   .directive('tickets', function () {
     return {      
       templateUrl: 'views/tickets.html',
-      restrict: 'E'/*,
+      restrict: 'E',
+      controller: function($scope, $element, $attrs) {
+
+     	$scope.lastTickets = [
+	      {id: 0, name: 'HTML5 Boilerplate', start: '2011-06-16', finish: '2011-06-16'}, //  ISO 8601 yyyy-MM-dd
+	      {id: 1, name: 'AngularJS', start: '2011-06-16', finish: '2011-06-16'},                      
+	      {id: 2, name: 'AngularJS', start: '2011-05-24', finish: '2011-05-24'},
+	      {id: 3, name: 'Karma', start: '2011-07-15'},      
+	      {id: 4, name: 'AngularJS', start: '2012-06-01', finish: '2012-06-01'},
+	      {id: 5, name: 'Karma', start: '2013-06-08', finish: '2013-06-08'}, 
+	      {id: 6, name: 'HTML5 Boilerplate', start: '2013-06-09', finish: '2013-06-09'}
+	    ];
+	    
+      }
+      /*,
       link: function postLink(scope, element, attrs) {
         //element.text('This is the tickets directive!');
       }*/
